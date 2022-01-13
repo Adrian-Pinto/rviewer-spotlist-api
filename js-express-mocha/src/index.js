9751b756a3588fc6b5d39e01387e81aca1f1031e
Adrian Pinto <adripintogonzalez@gmail.com>
2022-01-14 00:05:55 +0100

    add minimal api code to src/index.js add type: module propertie to package.json

--- js-express-mocha/src/index.js
+++ js-express-mocha/src/index.js
@@ -1,3 +1,14 @@
-/*
-    Your solution should go here
-*/
+import { stdout } from 'process';
+import express from 'express';
+import morgan from 'morgan';
+
+const api = express();
+const port = 3001;
+
+api.use(morgan('dev'));
+api.use(express.json());
+
+api.listen(
+  port,
+  () => stdout.write(`Server runin on port: ${port}`),
+);
