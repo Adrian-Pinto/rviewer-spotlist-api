7269237ffa836afeceeb588b65912d6b5ff46cac
Adrian Pinto <adripintogonzalez@gmail.com>
2022-01-14 14:19:21 +0100

    add userId propertie and Regex pattern to check it on models/usersModel.js

--- js-express-mocha/src/models/usersModel.js
+++ js-express-mocha/src/models/usersModel.js
@@ -5,6 +5,10 @@ const ajv = new Ajv();
 const schema = {
   type: 'object',
   properties: {
+    userId: {
+      type: 'string',
+      pattern: '[a-z0-9]{6}-[a-z0-9]{12}-[a-z0-9]{6}',
+    },
     name: {
       type: 'string',
       minLength: 3,
@@ -12,6 +16,7 @@ const schema = {
     password: { type: 'string' },
   },
   required: [
+    'userId',
     'name',
     'password',
   ],
