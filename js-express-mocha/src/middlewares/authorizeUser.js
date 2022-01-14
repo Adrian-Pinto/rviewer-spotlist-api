d314b3ea0423ba5b82b1228707f3433916bfadbd
Adrian Pinto <adripintogonzalez@gmail.com>
2022-01-14 13:12:58 +0100

    import userSchema from models/usersModel.js on middlewares/authorizeUser.js modifi export default function to check user auth data over userSchema

--- js-express-mocha/src/middlewares/authorizeUser.js
+++ js-express-mocha/src/middlewares/authorizeUser.js
@@ -1,16 +1,18 @@
+import userSchema from '../models/usersModel.js';
 import doHash from '../utils/doHash.js';
 import db from '../config/lowdbConfig.js';
 
 export default async (req, res, next) => {
   const { userId } = req.params;
   const { authorization } = req.headers;
+  const [name, password] = Buffer
+    .from(
+      authorization?.split(' ')[1].toString() || '',
+      'base64',
+    ).toString('ascii').split(':');
 
-  if (authorization) {
-    const hashPassword = doHash(Buffer
-      .from(
-        authorization.split(' ')[1].toString(),
-        'base64',
-      ).toString('ascii').split(':')[1]);
+  if (authorization && userSchema({ name, password })) {
+    const hashPassword = doHash(password);
     const isUser = db.data.users.find((user) => user.id === userId);
 
     if (!isUser) res.tatus(401).send('User not found with this id (or user is not the one authenticated)');
