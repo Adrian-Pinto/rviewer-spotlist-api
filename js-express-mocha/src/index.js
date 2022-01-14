d2ddf11b004a9cf5b44a3e43c181275c65c12225
Adrian Pinto <adripintogonzalez@gmail.com>
2022-01-14 01:46:20 +0100

    import routers on index.js rename songsRouter.js to sognsRouter.js

--- js-express-mocha/src/index.js
+++ js-express-mocha/src/index.js
@@ -1,6 +1,8 @@
 import { stdout } from 'process';
 import express from 'express';
 import morgan from 'morgan';
+import listsRouter from './controllers/listsRouter.js';
+import sognsRouter from './controllers/sognsRouter.js';
 import './config/lowdbConfig.js';
 
 const api = express();
@@ -8,6 +10,9 @@ const port = 3001;
 
 api.use(morgan('dev'));
 api.use(express.json());
+
+api.use('/api/v1/users', listsRouter);
+api.use('/api/v1/users', sognsRouter);
 
 api.listen(
   port,
