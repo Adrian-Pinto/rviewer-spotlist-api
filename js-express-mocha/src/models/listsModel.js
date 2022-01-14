c1f1a0ceb8a0f0fadf2e51993ed6744eb107323f
Adrian Pinto <adripintogonzalez@gmail.com>
2022-01-14 18:37:29 +0100

    fix some list bad properties causing a malfunction

--- js-express-mocha/src/models/listsModel.js
+++ js-express-mocha/src/models/listsModel.js
@@ -5,35 +5,20 @@ const ajv = new Ajv();
 const schema = {
   type: 'object',
   properties: {
-    listId: {
+    name: {
       type: 'string',
     },
     sogns: {
       type: 'array',
-      contains: [
+      items: [
         {
           type: 'object',
-          properties: {
-            artist: {
-              type: 'string',
-              minLength: 1,
-            },
-            title: {
-              type: 'string',
-              minLength: 1,
-            },
-          },
-          required: [
-            'artist',
-            'title',
-          ],
         },
       ],
-      minContains: 1,
     },
   },
   required: [
-    'listId',
+    'name',
   ],
 };
 
