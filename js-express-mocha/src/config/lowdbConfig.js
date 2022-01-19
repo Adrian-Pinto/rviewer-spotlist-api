676533e7f2b7a03a50b80c74d0c4532ff86c6c32
Adrian Pinto <adripintogonzalez@gmail.com>
2022-01-19 14:04:57 +0100

    add arg --NODE_ENV=test on package.json to choise db if is test o not add the necessary logic to lowdbConfig.json to choise the correct adapter depend of entry arg  - if NODE_ENV=test chose MemorySync adapter  - if !NODE_env=test chose JSONFileSync adapter add testDb.json on src/test/test.db

--- js-express-mocha/src/config/lowdbConfig.js
+++ js-express-mocha/src/config/lowdbConfig.js
@@ -1,6 +1,10 @@
-import { LowSync, JSONFileSync } from 'lowdb';
+import { LowSync, JSONFileSync, MemorySync } from 'lowdb';
 
-const db = new LowSync(new JSONFileSync('./database/db.json'));
+const isTestEnv = !!process.argv.find((argv) => argv === '--NODE_ENV=test')?.split('=')[1];
+
+const db = isTestEnv
+  ? new LowSync(new MemorySync())
+  : new LowSync(new JSONFileSync('./database/db.json'));
 
 db.read();
 db.data ||= {
